58f45acedb454aa3ffd6842a67ef1de2598017ea
kenedi <kheather@wpi.edu>
2017-02-23 17:14:15 -0500

    Added an activity for the log in

---
+++ app/src/main/java/com/example/kenedi/feedyoself/FeedYoSelfLogIn.java
@@ -0,0 +1,13 @@
+package com.example.kenedi.feedyoself;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedYoSelfLogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_yo_self_log_in);
+    }
+}
